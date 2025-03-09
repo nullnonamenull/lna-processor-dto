f575a928cb27969d81f1f3916c8779ba4f770337
Piotras <poleszak@protonmail.com>
2025-03-09 16:06:33 +0100

    Changes

--- lna-processor-dto/src/main/java/com/noname/lnaprocessordto/MessageRequestDTO.java
+++ lna-processor-dto/src/main/java/com/noname/lnaprocessordto/MessageRequestDTO.java
@@ -3,10 +3,13 @@ package com.noname.lnaprocessordto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 @Builder
 public class MessageRequestDTO {
 
+    private UUID sessionId;
     private String message;
 
 }
